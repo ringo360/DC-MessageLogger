9014514e8de9573e356425b43c00b6740dc2baea
ringoXD <ringoxd@ringoxd.dev>
2024-07-01 19:00:22 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { Client as Bot, Intents, TextChannel } from 'discord.js';
+import { Client as SelfBot } from 'discord.js-selfbot-v13';
+import config from '../config.json';
+import consola from 'consola';
+
+const bot = new Bot({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const selfbot = new SelfBot();
+
+bot.on('ready', async () => {
+	consola.success(`${bot.user?.username} is Ready!`);
+});
+
+selfbot.on('ready', async () => {
+	consola.success(`${selfbot.user?.username} is Ready!`);
+});
+
+selfbot.on('messageDelete', async (msg) => {
+	// console.log(msg);
+	consola.log('Detected - Check 1');
+	if (msg.author?.bot) return;
+	consola.log('Check 2');
+	if (msg.content) consola.log('Content: yes => ' + msg.content);
+	if (!msg.content || (msg.content && !(msg.content.length > 0))) return;
+	consola.log(
+		`Deleted message: ${msg.guild?.name}(${msg.guild?.id}) - ${msg.author?.username}(displayname: ${msg.author?.displayName}) said ${msg.content}`
+	);
+	//prettier-ignore
+	const channel = bot.channels.cache.get(config.guild.loggingChannel) as TextChannel
+	await channel.send(
+		`Deleted message: ${msg.guild?.name}(${msg.guild?.id}) - ${msg.author?.username}(displayname: ${msg.author?.displayName}) said ${msg.content}\nhttps://discord.com/channels/${msg.guildId}/${msg.channelId}/${msg.id}`
+	);
+	// consola.info(msg);
+});
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
+
+async function shutdown() {
+	consola.info('Shutting down...');
+	bot.destroy();
+	selfbot.destroy();
+	consola.success('Goodbye!');
+}
+
+process.on('uncaughtException', function (e) {
+	consola.error(e);
+});
+
+bot.login(config.tokens.bot);
+selfbot.login(config.tokens.selfbot);
